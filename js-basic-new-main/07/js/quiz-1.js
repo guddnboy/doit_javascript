fb3520ddb3ca4c2c49380ead5798d250e2456d0c
guddnboy <hung3018@naver.com>
2024-01-08 18:19:42 +0900

    0108 [07 QUIZ]

--- js-basic-new-main/07/js/quiz-1.js
+++ js-basic-new-main/07/js/quiz-1.js
@@ -1,11 +1,17 @@
 		var numbers = [2, 4, 6, 8, 10];
 		showArray(numbers);
 
+
 		function showArray(arr) {
 			var str = "<table><tr>";
+			var sum = 0;
 			for (var i=0; i<arr.length; i++) {
+				sum += numbers[i];
 				str += "<td>" + arr[i] + "</td>";
 			}
+			
+			arr.push(sum);
+			str += "<td>" + arr[i] + "</td>";
 			str += "</tr></table>";
 			document.write(str);
 		}
